5f4a22bf30f9521fefb96113ecd4ef84d1817d6b
Marcos Santana <brmarcossantanabr@gmail.com>
2023-05-17 12:23:25 -0300

    add effect transition on click

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -23,30 +23,33 @@ class Dashboard extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const ContactList()));
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  color: Theme.of(context).primaryColor,
-                  child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.people,
-                          color: Colors.white,
-                          size: 24.0,
-                        ),
-                        Text(
-                          'Contacts',
-                          style: TextStyle(color: Colors.white, fontSize: 16.0),
-                        ),
-                      ]),
+              child: Material(
+                color: Theme.of(context).primaryColor,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const ContactList()));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    height: 100,
+                    width: 150,
+                    child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.people,
+                            color: Colors.white,
+                            size: 24.0,
+                          ),
+                          Text(
+                            'Contacts',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 16.0),
+                          ),
+                        ]),
+                  ),
                 ),
               ),
             )
