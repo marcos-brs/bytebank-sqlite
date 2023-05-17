44c88e40f3e3489c28b2d947e035b8fdb8ef3620
Marcos Santana <brmarcossantanabr@gmail.com>
2023-05-17 10:39:14 -0300

    adjust card size

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,8 @@ class BytebankApp extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  height: 120,
-                  width: 100,
+                  height: 100,
+                  width: 150,
                   color: Colors.green,
                   child: const Column(children: [
                     Icon(Icons.people),
