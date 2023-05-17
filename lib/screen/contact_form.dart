efcd772902d3561c2cdb2693cedeb36ba96f20e5
Marcos Santana <brmarcossantanabr@gmail.com>
2023-05-17 12:57:54 -0300

    make contact form screen layout

--- lib/screen/contact_form.dart
+++ lib/screen/contact_form.dart
@@ -10,6 +10,58 @@ class ContactForm extends StatelessWidget {
         title: const Text('New contact'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Full name',
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                labelStyle: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              style: const TextStyle(
+                fontSize: 24.0,
+              ),
+              keyboardType: TextInputType.name,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Account number',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                style: const TextStyle(fontSize: 24.0),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Create'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
