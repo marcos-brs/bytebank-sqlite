d793a27be1418dbceb94fc541617f62cc73b4304
Marcos Santana <brmarcossantanabr@gmail.com>
2023-05-17 10:38:43 -0300

    add padding on card

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,17 @@ class BytebankApp extends StatelessWidget {
                 padding: const EdgeInsets.all(8.0),
                 child: Image.asset('images/bytebank_logo.png'),
               ),
-              Container(
-                height: 120,
-                width: 100,
-                color: Colors.green,
-                child: const Column(children: [
-                  Icon(Icons.people),
-                  Text('Contacts'),
-                ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 120,
+                  width: 100,
+                  color: Colors.green,
+                  child: const Column(children: [
+                    Icon(Icons.people),
+                    Text('Contacts'),
+                  ]),
+                ),
               )
             ],
           )),
