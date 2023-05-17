1ca0fe1319ac62d78d6968b37138993e4ed22118
Marcos Santana <brmarcossantanabr@gmail.com>
2023-05-17 10:36:57 -0300

    add logo

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,23 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dashboard'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Dashboard'),
+          ),
+          body: Column(
+            children: [
+              Image.asset('images/bytebank_logo.png'),
+              Container(
+                height: 120,
+                width: 100,
+                color: Colors.green,
+                child: const Column(children: [
+                  Icon(Icons.people),
+                  Text('Contacts'),
+                ]),
+              )
+            ],
+          )),
     );
   }
 }
